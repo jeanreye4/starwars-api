8ecfea1728bf3acdd8ff6c7d68a7f679ab4890b5
Jean Reyes <jeanreyes@NTC-MBA13-CA1475.local>
2022-10-31 14:47:06 -0400

    final final update

--- seed/seed.js
+++ seed/seed.js
@@ -3,7 +3,7 @@ import data from "../db/data.json" assert { type: "json" };
 import Anime from "../models/Anime.js";
 
 const insertData = async () => {
-  await Anime.deleteMany();
+  await db.dropDatabase();
   await Anime.insertMany(data);
   await db.close();
 };
