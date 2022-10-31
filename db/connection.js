089b4b06c8ab4b667b9e3f103107669edd5cff9d
Jean Reyes <jeanreyes@NTC-MBA13-CA1475.local>
2022-10-31 14:25:07 -0400

    update again

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,21 @@ import mongoose from "mongoose";
 
 let mongooseConfig = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}
-const connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/animeDB'
+  useUnifiedTopology: true,
+};
+const connectionString =
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/animeDB";
 
-mongoose.connect(connectionString, mongooseConfig)
+mongoose.connect(connectionString, mongooseConfig);
 
-mongoose.connection.on("connected", () => console.log("connected to database!"))
-mongoose.connection.on("disconnected", () => console.log("Disconnected to database!"))
-mongoose.connection.on("error", error => console.error("Error! look for:", error))
+mongoose.connection.on("connected", () =>
+  console.log("connected to database!")
+);
+mongoose.connection.on("disconnected", () =>
+  console.log("Disconnected to database!")
+);
+mongoose.connection.on("error", (error) =>
+  console.error("Error! look for:", error)
+);
 
-export default mongoose.connection
+export default mongoose.connection;
