b544a48432b814d3e2545eb00c1e1c946494ff1d
Jean Reyes <jeanreyes@NTC-MBA13-CA1475.local>
2022-10-31 14:02:51 -0400

    ready to deploy

--- seed/seed.js
+++ seed/seed.js
@@ -1,11 +1,11 @@
-import db from "../db/connection.js"
-import data from "../db/data.json" assert {type: "json"}
-import Anime from "../models/Anime.js"
+import db from "../db/connection.js";
+import data from "../db/data.json" assert { type: "json" };
+import Anime from "../models/Anime.js";
 
 const insertData = async () => {
-  await Anime.deleteMany({})
-  await Anime.insertMany(data)
-  db.close()
-}
+  await Anime.deleteMany({});
+  await Anime.insertMany(data);
+  db.close();
+};
 
-insertData()
+insertData();
