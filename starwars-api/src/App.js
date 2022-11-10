a31b5d7f98562d9f9cf687410f1ac76a4877fffc
Jean Reyes <jeanreyes@NTC-MBA13-CA1475.local>
2022-11-10 15:57:02 -0500

    my actual project

---
+++ starwars-api/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {useEffect, useState } from "react"
+import { json } from 'react-router-dom';
+
+function App() {
+  const [starwars, setStarwars] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await fetch(
+        "https://apiproject-production-3ac3.up.railway.app/characters"
+      );
+      const json = await response.json();
+      setStarwars(json);
+    };
+    fetchCharacters();
+  }, []);
+
+  console.log(starwars)
+  
+  return (
+    <>
+      <h1>Starwars Info</h1>
+      <div className='boxes'>
+        {starwars.map((starwars) => ( 
+          <div className='information'>
+            <h3>{starwars.name}</h3> 
+            <ul>
+            <li>{starwars.gender}</li>
+            <li>{starwars.birth_year}(year born)</li>
+            <li>{starwars.height}(height cm)</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
