b17098e08b0f1b76a1816688dd28d7b9d092a1b7
Jean Reyes <jeanreyes@NTC-MBA13-CA1475.local>
2022-11-10 09:10:17 -0500

    Starwars-api

--- models/Anime.js
+++ models/Anime.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 let Anime = new Schema({
   anime: String,
   character: String,
-  quote: String
+  quote: String,
+  img: String
 })
 
 export default mongoose.model("animes", Anime)
